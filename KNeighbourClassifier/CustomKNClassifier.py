ee8ab0aba68cae42eaf1acb13e9387bc5385fea7
Shameer Eranhikath <shmr_ekth@yahoo.com>
2020-06-14 17:57:58 +0400

    Install and import sklearn

---
+++ KNeighbourClassifier/CustomKNClassifier.py
@@ -0,0 +1,8 @@
+from sklearn import datasets
+from sklearn.model_selection import  train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+from scipy.spatial import distance
+
+
+
